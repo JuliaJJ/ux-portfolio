ef828ef7bd72d760efe1c2d8ab18e66405f63c31
Julie Johnston <julie.johnston@gmail.com>
2023-07-22 13:48:29 -0700

    add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap(), mdx()]
 });
