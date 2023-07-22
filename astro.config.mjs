953e30662cdff46e12296626cb005c0ef60f25e6
Julie Johnston <julie.johnston@gmail.com>
2023-07-22 13:51:23 -0700

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import mdx from "@astrojs/mdx";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), mdx()]
+  integrations: [tailwind(), sitemap(), mdx()],
+  output: "server",
+  adapter: netlify()
 });
